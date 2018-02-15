d18191421007e388a3a8aab2a8a8227d237d210a
Patrick Emond <emondpd@mcmaster.ca>
2018-02-15 14:30:37 -0500

    Restricting applicant's to their own requisitions

--- src/service/requisition/module.class.php
+++ src/service/requisition/module.class.php
@@ -17,9 +17,42 @@ class module extends \cenozo\service\module
   /**
    * Extend parent method
    */
+  public function validate()
+  {
+    parent::validate();
+
+    $session = lib::create( 'business\session' );
+    $db_user = $session->get_user();
+    $db_role = $session->get_role();
+
+    if( 300 > $this->get_status()->get_code() )
+    {
+      // make sure to restrict applicants to their own requisitions
+      $db_requisition = $this->get_resource();
+      if( 'applicant' == $db_role->name && !is_null( $db_requisition ) )
+      {
+        if( $db_requisition->user_id != $db_user->id )
+        {
+          $this->get_status()->set_code( 404 );
+          return;
+        }
+      }
+    }
+  }
+
+  /**
+   * Extend parent method
+   */
   public function prepare_read( $select, $modifier )
   {
     parent::prepare_read( $select, $modifier );
+
+    $session = lib::create( 'business\session' );
+    $db_user = $session->get_user();
+    $db_role = $session->get_role();
+
+    // only show applicants their own requisitions
+    if( 'applicant' == $db_role->name ) $modifier->where( 'requisition.user_id', '=', $db_user->id );
 
     if( $select->has_column( 'user_full_name' ) )
     {
