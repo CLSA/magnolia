265712437475dfcb2bab8d351cd4f7d0ebb25fe0
Patrick Emond <emondpd@mcmaster.ca>
2018-03-02 17:15:11 -0500

    Implementing progress reports [WIP]

--- web/app/progress_report/module.js
+++ web/app/progress_report/module.js
@@ -102,9 +102,81 @@ define( function() {
               }
             },
             a1: {
-              tab: {
-                en: 'Annual Report',
-                fr: 'Rapport annuel',
+              tab: { en: 'Annual Report', fr: 'Rapport annuel' },
+              title: { en: 'Accomplishments', fr: 'Réalisations' },
+              text: {
+                en: 'The information reported in this section allows the CLSA to assess what progress has been made toward accomplishing the objectives set out in the initial application and the impact of the project towards the advancement of knowledge.',
+                fr: 'Les informations transmises ci-dessous permettent à l’ÉLCV d’évaluer les progrès effectués qui mènent à l’atteinte des objectifs définis dans la demande initiale, et de mesurer l’impact du projet pour l’avancement des connaissances.'
+              },
+              a: {
+                question: {
+                  en: 'What was accomplished for this reporting period?',
+                  fr: 'Quels sont les progrès réalisés pendant la période qui fait l’objet du rapport?'
+                },
+                activities: {
+                  en: 'Major activities',
+                  fr: 'Les activités importantes'
+                },
+                findings: {
+                  en: 'Major findings, developments, or conclusions (both positive and negative)',
+                  fr: 'Les observations, avancées ou conclusions importantes (positives et négatives)'
+                },
+                outcomes: {
+                  en: 'Key outcomes or other achievements',
+                  fr: 'Les principaux résultats et autres réalisations'
+                }
+              },
+              b: {
+                question: {
+                  en: 'If this is a trainee project that has been granted a fee waiver, you must complete the information below. (Any publications must be reported in section ‘c’)',
+                  fr: 'S’il s’agit du projet d’un stagiaire qui a bénéficié d’une exonération des frais, vous devez répondre au questions suivantes. (Les publications doivent être déclarées à la section « c »)'
+                },
+                thesis_title: {
+                  en: 'Thesis title (if graduate student trainee)',
+                  fr: 'Titre de la thèse (s’il s’agit d’un étudiant aux cycles supérieurs)'
+                },
+                thesis_status: {
+                  en: 'Thesis status (Proposal in preparation, thesis in progress, submitted, approved)',
+                  fr: 'État de la thèse (Proposition en cours de préparation, thèse en cours d’écriture, thèse déposée, thèse approuvée)'
+                }
+              },
+              c: {
+                question: {
+                  en: 'What has the project produced?',
+                  fr: 'Qu’est-ce que votre projet a produit?'
+                },
+                text: {
+                  en: 'List any products resulting from the project during the reporting period. Please provide references where available, and for peer-reviewed publications please specify if ‘in press’, ‘submitted’ or ‘published’. If you have not yet done so, please provide a copy of peer-reviewed publications to the CLSA when submitting this report.',
+                  fr: 'Énumérer les produits qui ont été développés dans le cadre de votre projet pendant la période en question.  Veuillez fournir les références lorsqu’elles sont disponibles. Pour les articles évalués par des pairs, veuillez spécifier s’ils sont « sous presse », « soumis » ou « publiés ». Si ce n’est pas déjà fait, veuillez fournir une copie des articles évalués par des pairs à l’ÉLCV en soumettant ce rapport.'
+                },
+                publication: {
+                  en: 'Peer-reviewed publications - Format: Author, A. (Publication Year). Article title. Periodical Title, Volume (Issue), pp.-pp.',
+                  fr: 'Publications évaluées par des pairs - Format : Auteur, A. (année de publication). Titre de l’article. Titre du périodique, Volume (numéro), pp.-pp.'
+                },
+                conference: {
+                  en: 'Conference papers and presentations - Format: Author, A. (Publication Year). Title of Paper or Proceedings, Title of Conference, Location, Date.  Place of publication: Publisher.',
+                  fr: 'Actes de conférence et présentations - Format : Auteur, A. (année de publication). Titre de l’article ou de l’acte, titre de la conférence, lieu, date.  Lieu de publication : Éditeur.'
+                },
+                mass_media: {
+                  en: 'Mass media - Format: Author, A. (Publication Year). Article title. Retrieved from (URL) on (Date).',
+                  fr: 'Médias - Format : Auteur, A. (année de publication). Titre de l’article. Tiré de (URL) le (Date).'
+                },
+                technology: {
+                  en: 'Website(s), technologies, equipment or techniques',
+                  fr: 'Site(s) Web, technologies, de l’équipement ou techniques'
+                },
+                invention: {
+                  en: 'Inventions, patent applications,or licenses',
+                  fr: 'Inventions, demandes de brevet ou de licence'
+                },
+                data: {
+                  en: 'Derived variables, models, databases, audio or video products, software, educational aids or curricula',
+                  fr: 'Variables dérivées, modèles, bases de données, documents audio ou vidéo, logiciels, outils ou programmes éducatifs'
+                },
+                collaboration: {
+                  en: 'Collaborations - partnerships with academic institutions, non-profits, industrial or commercial firms, governments, or school systems.',
+                  fr: 'Collaborations - partenariats avec des établissements universitaires, des organismes à but non lucratif, des entreprises industrielles ou commerciales, des gouvernements ou les milieux scolaires'
+                }
               }
             }
           };
@@ -131,8 +203,123 @@ define( function() {
                 en: 'Consult us for any questions regarding the final report at <a href="mailto:access@clsa-elcv.ca">access@clsa-elcv.ca</a>.',
                 fr: 'Veuillez adresser toute question relative au rapport final à <a href="mailto:access@clsa-elcv.ca">access@clsa-elcv.ca</a>.'
               }
+            },
+            a1: {
+              tab: {
+                en: 'Final Report',
+                fr: 'Rapport final'
+              },
+              title: {
+                en: 'Accomplishments',
+                fr: 'Réalisations'
+              },
+              text: {
+                en: 'The information reported in this section allows the CLSA to assess what progress has been made toward accomplishing the objectives set out in the initial application and the impact of the project towards the advancement of knowledge.',
+                fr: 'Les informations transmises ci-dessous permettent à l’ÉLCV d’évaluer les progrès effectués qui mènent à l’atteinte des objectifs définis dans la demande initiale, et de mesurer l’impact du projet pour l’avancement des connaissances.'
+              },
+              a: {
+                question: {
+                  en: 'What was accomplished in this study?',
+                  fr: 'Quelles sont les réalisations de l’étude?'
+                },
+                activities: {
+                  en: 'Major activities',
+                  fr: 'Les activités importantes'
+                },
+                findings: {
+                  en: 'Major findings, developments, or conclusions (both positive and negative)',
+                  fr: 'Les observations, avancées ou conclusions importantes (positives et négatives)'
+                },
+                outcomes: {
+                  en: 'Key outcomes or other achievements',
+                  fr: 'Les principaux résultats et autres réalisations'
+                }
+              },
+              b: {
+                question: {
+                  en: 'If this is a trainee project that has been granted a fee waiver, you must complete the information below. (Any publications must be reported in section ‘c’)',
+                  fr: 'S’il s’agit du projet d’un stagiaire qui a bénéficié d’une exonération des frais, vous devez répondre au questions suivantes. (Les publications doivent être déclarées à la section « c »)'
+                },
+                thesis_title: {
+                  en: 'Thesis title (if graduate student trainee)',
+                  fr: 'Titre de la thèse (s’il s’agit d’un étudiant aux cycles supérieurs)'
+                },
+                thesis_status: {
+                  en: 'Thesis status (Proposal in preparation, thesis in progress, submitted, approved)',
+                  fr: 'État de la thèse (Proposition en cours de préparation, thèse en cours d’écriture, thèse déposée, thèse approuvée)'
+                }
+              },
+              c: {
+                question: {
+                  en: 'What has the project produced?',
+                  fr: 'Qu’est-ce que votre projet a produit?'
+                },
+                text: {
+                  en: 'List any products resulting from the project during the reporting period. Please provide references where available, and for peer-reviewed publications please specify if ‘in press’, ‘submitted’ or ‘published’. If you have not yet done so, please provide a copy of peer-reviewed publications to the CLSA when submitting this report.',
+                  fr: 'Énumérer les produits qui ont été développés dans le cadre de votre projet pendant la période en question.  Veuillez fournir les références lorsqu’elles sont disponibles. Pour les articles évalués par des pairs, veuillez spécifier s’ils sont « sous presse », « soumis » ou « publiés ». Si ce n’est pas déjà fait, veuillez fournir une copie des articles évalués par des pairs à l’ÉLCV en soumettant ce rapport.'
+                },
+                publication: {
+                  en: 'Peer-reviewed publications - Format: Author, A. (Publication Year). Article title. Periodical Title, Volume (Issue), pp.-pp.',
+                  fr: 'Publications évaluées par des pairs - Format : Auteur, A. (année de publication). Titre de l’article. Titre du périodique, Volume (numéro), pp.-pp.'
+                },
+                conference: {
+                  en: 'Conference papers and presentations - Format: Author, A. (Publication Year). Title of Paper or Proceedings, Title of Conference, Location, Date.  Place of publication: Publisher.',
+                  fr: 'Actes de conférence et présentations - Format : Auteur, A. (année de publication). Titre de l’article ou de l’acte, titre de la conférence, lieu, date.  Lieu de publication : Éditeur.'
+                },
+                mass_media: {
+                  en: 'Mass media - Format: Author, A. (Publication Year). Article title. Retrieved from (URL) on (Date).',
+                  fr: 'Médias - Format : Auteur, A. (année de publication). Titre de l’article. Tiré de (URL) le (Date).'
+                },
+                technology: {
+                  en: 'Website(s), technologies, equipment or techniques',
+                  fr: 'Site(s) Web, technologies, de l’équipement ou techniques'
+                },
+                invention: {
+                  en: 'Inventions, patent applications,or licenses',
+                  fr: 'Inventions, demandes de brevet ou de licence'
+                },
+                data: {
+                  en: 'Derived variables, models, databases, audio or video products, software, educational aids or curricula',
+                  fr: 'Variables dérivées, modèles, bases de données, documents audio ou vidéo, logiciels, outils ou programmes éducatifs'
+                },
+                collaboration: {
+                  en: 'Collaborations - partnerships with academic institutions, non-profits, industrial or commercial firms, governments, or school systems.',
+                  fr: 'Collaborations - partenariats avec des établissements universitaires, des organismes à but non lucratif, des entreprises industrielles ou commerciales, des gouvernements ou les milieux scolaires'
+                }
+              },
+              d: {
+                question: {
+                  en: 'What is the impact of the project?',
+                  fr: 'Quel est l’impact du projet?'
+                },
+                text: {
+                  en: 'Describe distinctive contributions, major accomplishments, innovations, successes, or any change in practice or behavior that has come about as a result of the project.',
+                  fr: 'Décrivez les contributions marquantes, les réalisations, innovations, succès importants ou les changements dans la pratique ou les comportements qui ont émergé des conclusions de votre projet.'
+                }
+              },
+              e: {
+                question: {
+                  en: 'What opportunities for training and professional development has the project provided?',
+                  fr: 'Quelles occasions de formation et de perfectionnement professionnel votre projet a-t-il offert?'
+                },
+                text: {
+                  en: 'List any opportunities for training (i.e. student research assistants) and professional development (i.e. HQP development opportunities, lectures, courses) that the project has provided.',
+                  fr: 'Énumérez toutes les occasions de formation et de perfectionnement professionnel que votre projet a offertes.'
+                }
+              },
+              f: {
+                question: {
+                  en: 'How have the results been disseminated to communities of interest?',
+                  fr: 'Comment avez-vous diffusé les résultats aux groupes d’intérêt?'
+                },
+                text: {
+                  en: 'Describe any knowledge translation and outreach activities that have been undertaken.',
+                  fr: 'Décrivez toutes les activités d’application des connaissances et de sensibilisation qui ont été réalisées.'
+                }
+              }
             }
           };
+
         }
       };
     }
