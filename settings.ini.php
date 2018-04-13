e32740bea3ae800c60214ed26780acca6cfd6798
Patrick Emond <emondpd@mcmaster.ca>
2018-04-13 16:37:52 -0400

    Hiding ethics letter upload button when ethics is not 'yes'

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.2';
-$SETTINGS['general']['build'] = 'b5d761a';
+$SETTINGS['general']['build'] = 'abe8478';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
