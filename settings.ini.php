933c0d5a5059df4ff4940f7fd8b6cf0d130715e3
Patrick Emond <emondpd@mcmaster.ca>
2018-09-18 16:41:37 -0400

    Making French text italic

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.2';
-$SETTINGS['general']['build'] = 'b774b9d';
+$SETTINGS['general']['build'] = 'f64f1eb';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
