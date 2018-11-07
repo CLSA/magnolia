4ef933eef5f809e52c712d39908e06b2afedc5b7
Patrick Emond <emondpd@mcmaster.ca>
2018-11-07 10:05:37 -0500

    Adding missing TODO text

--- db/upgrade/2.2/notification_type.sql
+++ db/upgrade/2.2/notification_type.sql
@@ -16,9 +16,17 @@ ENGINE = InnoDB;
 INSERT IGNORE INTO notification_type( name, title_en, title_fr, message_en, message_fr ) VALUES
 (
   "Action required",
-  "TODO: title",
+  "CLSA Data Access Application – ACTION REQUIRED",
   "TRANSLATION REQUIRED",
-  "TODO: This will be the message sent to the applicant to indicate that an action is required.",
+  "Dear Applicant,
+
+An action is required on your recent application. Please log in to the CLSA online data application (requisition) software, Magnolia (https://magnolia.clsa-elcv.ca/live/gl/). Please look for the flashing heading(s) within the requisition and then the “Attention” box on the page(s). Follow the instructions given to remedy the issue. Once you are sure you have made ALL the revisions, please re-submit the application.
+
+Your re-submitted application will continue through the review process and you will be contacted with the outcome at a later date.
+
+If you have any questions related to data access, please contact us at access@clsa-elcv.ca.
+
+The CLSA Data Access Team",
   "TRANSLATION REQUIRED"
 ), (
   "Requisition Reactivated",
