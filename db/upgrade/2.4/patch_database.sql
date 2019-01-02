2808e65783cd0aed631d9983db205ede3d97a198
Patrick Emond <emondpd@mcmaster.ca>
2019-01-02 12:59:10 -0500

    Removing write access to report_restriction services

--- db/upgrade/2.4/patch_database.sql
+++ db/upgrade/2.4/patch_database.sql
@@ -1,4 +1,4 @@
--- Patch to upgrade database to version 2.3
+-- Patch to upgrade database to version 2.4
 
 SET AUTOCOMMIT=0;
 
@@ -11,6 +11,9 @@ SOURCE reference.sql
 SOURCE reqn_data_option.sql
 SOURCE service.sql
 SOURCE role_has_service.sql
+SOURCE report_type.sql
+SOURCE application_type_has_report_type.sql
+SOURCE role_has_report_type.sql
 
 SOURCE update_version_number.sql
 
