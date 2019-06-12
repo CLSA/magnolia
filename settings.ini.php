40929ec762b5ef3aa69f9099be4a3f36735828c3
Patrick Emond <emondpd@mcmaster.ca>
2019-06-12 15:52:10 -0400

    More fixing of the last bug

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.4';
-$SETTINGS['general']['build'] = '34878ae';
+$SETTINGS['general']['build'] = 'b14678f';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
