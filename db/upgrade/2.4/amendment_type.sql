4eae9f795ab4bf1a93eda2441ec6b2a64f8bb1ab
Patrick Emond <emondpd@mcmaster.ca>
2019-07-19 15:47:07 -0400

    Adding amendment types

--- db/upgrade/2.4/amendment_type.sql
+++ db/upgrade/2.4/amendment_type.sql
@@ -8,3 +8,10 @@ CREATE TABLE IF NOT EXISTS amendment_type (
   reason_fr VARCHAR(255) NOT NULL,
   PRIMARY KEY (id))
 ENGINE = InnoDB;
+
+INSERT IGNORE INTO amendment_type( reason_en, reason_fr ) VALUES
+( "Amendment to Primary Applicant Information", "TRANSLATION REQUIRED" ),
+( "Changes to Co-Applicants and Support Personnel", "TRANSLATION REQUIRED" ),
+( "Changes to the Approved Project Title, Lay Summary and/or Objectives", "TRANSLATION REQUIRED" ),
+( "Changes to the Project Timeline", "TRANSLATION REQUIRED" ),
+( "Changes to the Data Checklist.", "TRANSLATION REQUIRED" );
