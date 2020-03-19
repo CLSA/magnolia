169a0e952f030301752e9d9ec024277c55f38cf0
Patrick Emond <emondpd@mcmaster.ca>
2020-03-19 10:18:42 -0400

    Updating pdf_form SQL patch

--- db/upgrade/2.5/pdf_form.sql
+++ db/upgrade/2.5/pdf_form.sql
@@ -3,6 +3,26 @@ DELIMITER //
 CREATE PROCEDURE patch_pdf_form()
   BEGIN
 
+    SELECT "Adding new Co-Applicant Agreement PDF form" AS "";
+
+    SELECT COUNT(*) INTO @test
+    FROM pdf_form
+    JOIN pdf_form_type ON pdf_form.pdf_form_type_id = pdf_form_type.id
+    WHERE pdf_form_type.name = "Co-Applicant Agreement";
+
+    IF @test = 0 THEN
+      UPDATE pdf_form
+      JOIN pdf_form_type ON pdf_form.pdf_form_type_id = pdf_form_type.id
+      SET active = 0
+      WHERE pdf_form_type.name = "Co-Applicant Agreement"
+      AND version < DATE( NOW() );
+
+      INSERT IGNORE INTO pdf_form( pdf_form_type_id, version, active )
+      SELECT pdf_form_type.id, DATE( NOW() ), 1
+      FROM pdf_form_type
+      WHERE name = "Co-Applicant Agreement";
+    END IF;
+
     SELECT "Adding new Data Checklist PDF form" AS "";
 
     SELECT COUNT(*) INTO @test
@@ -10,7 +30,7 @@ CREATE PROCEDURE patch_pdf_form()
     JOIN pdf_form_type ON pdf_form.pdf_form_type_id = pdf_form_type.id
     WHERE pdf_form_type.name = "Data Checklist";
 
-    IF @test = 1 THEN
+    IF @test = 3 THEN
       UPDATE pdf_form
       JOIN pdf_form_type ON pdf_form.pdf_form_type_id = pdf_form_type.id
       SET active = 0
@@ -28,8 +48,3 @@ DELIMITER ;
 
 CALL patch_pdf_form();
 DROP PROCEDURE IF EXISTS patch_pdf_form;
-
-INSERT IGNORE INTO pdf_form( pdf_form_type_id, version, active )
-SELECT id, 1, 1
-FROM pdf_form_type
-WHERE name = "Co-Applicant Agreement";
