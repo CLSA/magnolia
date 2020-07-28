a3d86b8892720e4d0d30c7cd7cabfc9168a291f4
Patrick Emond <emondpd@mcmaster.ca>
2020-07-28 14:54:57 -0400

    Marking start of version 2.6

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.5';
+$SETTINGS['general']['version'] = '2.6';
 $SETTINGS['general']['build'] = '5c04644';
 
 // the location of magnolia internal path
