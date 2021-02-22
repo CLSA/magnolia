e2075fc2aa748269d27011c0fc59cb54712acd14
Patrick Emond <emondpd@mcmaster.ca>
2021-02-22 15:04:56 -0500

    Adding missing trigger to reqn table

--- db/upgrade/2.6/reqn.sql
+++ db/upgrade/2.6/reqn.sql
@@ -71,6 +71,7 @@ DROP TRIGGER IF EXISTS reqn_AFTER_INSERT $$
 CREATE DEFINER = CURRENT_USER TRIGGER reqn_AFTER_INSERT AFTER INSERT ON reqn FOR EACH ROW
 BEGIN
   CALL update_reqn_last_ethics_approval( NEW.id );
+  CALL update_reqn_current_final_report( NEW.id );
 END$$
 
 DELIMITER ;
