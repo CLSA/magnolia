237a0a771afadc37a7ade91db596cb88fd24601b
Patrick Emond <emondpd@mcmaster.ca>
2021-03-17 17:02:05 -0400

    Removing unused columns

--- db/upgrade/2.6/reqn_version.sql
+++ db/upgrade/2.6/reqn_version.sql
@@ -11,30 +11,6 @@ CREATE PROCEDURE patch_reqn_version()
       AND constraint_name = "fk_access_site_id"
     );
 
-    SELECT "Adding new agreement_start_date column to reqn_version table" AS "";
-
-    SELECT COUNT(*) INTO @test
-    FROM information_schema.COLUMNS
-    WHERE table_schema = DATABASE()
-    AND table_name = "reqn_version"
-    AND column_name = "agreement_start_date";
-
-    IF @test = 0 THEN
-      ALTER TABLE reqn_version ADD COLUMN agreement_start_date DATE NULL DEFAULT NULL AFTER agreement_filename;
-    END IF;
-
-    SELECT "Adding new agreement_end_date column to reqn_version table" AS "";
-
-    SELECT COUNT(*) INTO @test
-    FROM information_schema.COLUMNS
-    WHERE table_schema = DATABASE()
-    AND table_name = "reqn_version"
-    AND column_name = "agreement_end_date";
-
-    IF @test = 0 THEN
-      ALTER TABLE reqn_version ADD COLUMN agreement_end_date DATE NULL DEFAULT NULL AFTER agreement_start_date;
-    END IF;
-
     SET @sql = CONCAT(
       "SELECT study_phase.id INTO @study_phase_id ",
       "FROM ", @cenozo, ".study ",
