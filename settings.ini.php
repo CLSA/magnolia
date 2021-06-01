c387724479d86ff1817bdbdd5c9755b3f3f7319b
Patrick Emond <emondpd@mcmaster.ca>
2021-06-01 11:04:46 -0400

    Fixing incorrect missing ethics warning

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = 'eec9314';
+$SETTINGS['general']['build'] = 'ae7b83c';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
