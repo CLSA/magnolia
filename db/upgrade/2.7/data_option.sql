24883e7b6818e753e563c1426583f09db58664c1
Patrick Emond <emondpd@mcmaster.ca>
2021-07-28 11:36:04 -0400

    Fixing bug in db script

--- db/upgrade/2.7/data_option.sql
+++ db/upgrade/2.7/data_option.sql
@@ -1,6 +1,6 @@
 SELECT "Adding new cognition data-option" AS "";
 
 INSERT IGNORE INTO data_option( data_option_category_id, rank, justification, name_en, name_fr )
-SELECT data_option_category.id, 7, "Cognition (Raw data)", true, "Cognition (données brutes)"
+SELECT data_option_category.id, 7, true, "Cognition (Raw data)", "Cognition (données brutes)"
 FROM data_option_category
 WHERE name_en = "Additional Data";
