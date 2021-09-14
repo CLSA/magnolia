4ee4f5b6c9be0b6bcd4d97ddb13d293946416570
Patrick Emond <emondpd@mcmaster.ca>
2021-09-14 11:50:15 -0400

    Fixing bug in cn-deferral-note directive
    It seems that defining the input variable directly in the template no
    longer works.  The fix is to define it as a variable in the $scope and
    pass that variable instead.

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = 'ace5031';
+$SETTINGS['general']['build'] = 'e9838c2';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
