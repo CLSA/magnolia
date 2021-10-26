184a549856a198f9aab31a24828f11c08444f2fc
Patrick Emond <emondpd@mcmaster.ca>
2021-10-26 15:17:40 -0400

    Applying use of cenozoApp.extendModule

--- web/app/site/module.extend.js
+++ web/app/site/module.extend.js
@@ -1,10 +1,6 @@
-// extend the framework's module
-define( [ cenozoApp.module( 'site' ).getFileUrl( 'module.js' ) ], function() {
-  'use strict';
-
-  var module = cenozoApp.module( 'site' );
+cenozoApp.extendModule( { name: 'site', create: module => {
 
   // we don't need the participant_count column
   delete module.columnList.participant_count;
 
-} );
+} } );
