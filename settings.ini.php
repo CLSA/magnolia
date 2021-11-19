2ead9f8fee10011e130cee2b8a948dd734802095
Patrick Emond <emondpd@mcmaster.ca>
2021-11-19 12:50:31 -0500

    Reverting back to version 2.8

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.7';
+$SETTINGS['general']['version'] = '2.8';
 $SETTINGS['general']['build'] = '4918067';
 
 // the location of magnolia internal path
