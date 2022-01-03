faaacd2afa8fa891a71b447a2e18b5fa086b861b
Patrick Emond <emondpd@mcmaster.ca>
2022-01-03 10:41:28 -0500

    Making sure files exist before unlinking

--- src/database/reqn_version.class.php
+++ src/database/reqn_version.class.php
@@ -104,7 +104,7 @@ class reqn_version extends \cenozo\database\record
 
     parent::delete();
 
-    foreach( $file_list as $file ) unlink( $file );
+    foreach( $file_list as $file ) if( file_exists( $file ) ) unlink( $file );
   }
 
   /**
