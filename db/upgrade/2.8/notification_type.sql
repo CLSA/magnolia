682345a00086c2e0e717841b43a11aa8272d41bf
Patrick Emond <emondpd@mcmaster.ca>
2022-03-23 10:56:20 -0400

    Adding translations

--- db/upgrade/2.8/notification_type.sql
+++ db/upgrade/2.8/notification_type.sql
@@ -2,7 +2,15 @@ SELECT "Adding new notification types" AS "";
 
 INSERT IGNORE INTO notification_type SET
   name = "Unsubmitted Application Expiry Notice",
-  title_en = "CLSA Data Access - Noticy of Expiry",
-  title_fr = "Accès aux données de l’ÉLCV - TODO",
-  message_en = "TODO",
-  message_fr = "TODO";
+  title_en = "CLSA Data Access - Notice of Expiry",
+  title_fr = "Accès aux données de l’ÉLCV - Avis d’expiration",
+  message_en = "Dear Dr. {{applicant_name}},
+
+You have an outstanding temporary requisition in Magnolia, project number {{identifier}}. As this temporary requisition has had no action in 18 months we will be deleting it in Magnolia in approximately one (1) month. Once this has been completed you will no longer see {{identifier}} in your list of requisitions when you log into Magnolia.
+
+The CLSA Data Access Team",
+  message_fr = " Bonjour Dr / Dre {{applicant_name}}, 
+
+Vous avez une demande d’accès temporaire en attente dans Magnolia, numéro de projet {{identifier}}.  Comme cette demande d’accès temporaire n’a eu aucune suite depuis 18 mois, nous la supprimerons dans Magnolia dans environ un (1) mois. Une fois cette opération terminée, vous ne verrez plus {{identifier}} dans votre liste de demandes lorsque vous vous connecterez à Magnolia. 
+
+L’équipe d’accès aux données de l’ÉLCV";
