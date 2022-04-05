091f6f28c6115e7ba59ed1afee770873ffdf717e
Patrick Emond <emondpd@mcmaster.ca>
2022-04-05 11:37:55 -0400

    Changing instructions

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.8';
-$SETTINGS['general']['build'] = 'd4ece45';
+$SETTINGS['general']['build'] = 'd1f9ac9';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
