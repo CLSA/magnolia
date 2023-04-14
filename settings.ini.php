c70436634a459ab97eb30e409d6608e4c64c9e54
Patrick Emond <emondpd@mcmaster.ca>
2023-04-14 14:02:53 -0400

    Bumping to version 2.9

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.8';
+$SETTINGS['general']['version'] = '2.9';
 $SETTINGS['general']['build'] = '3668558';
 
 // the location of magnolia internal path
