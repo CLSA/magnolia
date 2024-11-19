99b7bce90aa512421c65176c7a57ad24a42e6b7d
Patrick Emond <emondpd@mcmaster.ca>
2024-11-19 10:28:00 -0500

    Tweak: fixed wrong version number

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.9';
+$SETTINGS['general']['version'] = '2.10';
 $SETTINGS['general']['build'] = '04744f0';
 
 // the location of magnolia internal path
