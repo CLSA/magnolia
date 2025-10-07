bf7b86e08de29fefb863609a2982e4919a4e00ac
Patrick Emond <emondpd@mcmaster.ca>
2025-10-07 11:24:57 -0400

    Feature: adding additional instruction text to application form

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = '5d440db';
+$SETTINGS['general']['build'] = 'ffc3aa8';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
