34c45f05cd88083a0a2402cdb452d2feaeb40de3
Patrick Emond <emondpd@mcmaster.ca>
2025-11-13 15:50:28 -0500

    Feature: adding additional help text

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = '213a9b8';
+$SETTINGS['general']['build'] = '247a93a';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
