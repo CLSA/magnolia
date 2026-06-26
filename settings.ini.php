2cc5ba8453ab663808f2a2285ab28974cd61d6fe
Patrick Emond <emondpd@mcmaster.ca>
2026-06-26 16:26:21 -0400

    Feature: adding new instructions to requisition form

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'magnolia';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '3.0';
-$SETTINGS['general']['build'] = '0f688ef';
+$SETTINGS['general']['build'] = 'd2d5f26';
 
 // the location of magnolia internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
