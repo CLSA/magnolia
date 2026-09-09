48c747b6f369c2b09efcfc593a92458c0ad828f8
Patrick Emond <emondpd@mcmaster.ca>
2026-09-09 15:47:38 -0400

    Bug: server-side was rejecting reqn start dates that are on the earliest possible day

--- src/service/reqn/patch.class.php
+++ src/service/reqn/patch.class.php
@@ -152,7 +152,8 @@ class patch extends \cenozo\service\patch
               $db_reqn->save(); // this will make sure the deadline is appropriate
               if( $db_reqn->deadline_id )
               {
-                $deadline = util::get_datetime_object( $db_reqn->get_deadline()->datetime );
+                $deadline = clone $db_reqn->get_deadline()->datetime;
+                $deadline->setTime( 0, 0 );
                 $deadline->add( new \DateInterval( sprintf( 'P%dM', $delay ) ) );
                 if( $db_amendment->get_current_reqn_version()->start_date < $deadline ) $code = 409;
               }
